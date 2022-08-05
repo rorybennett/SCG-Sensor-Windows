e8df299919f56e3056f49e8ed1c5bdc3501cdd50
Rory <rorybennett123@gmail.com>
2022-08-05 08:41:53 +0100

    add Layout class

---
+++ Layout.py
@@ -0,0 +1,30 @@
+import PySimpleGUI as sg
+
+
+class Layout:
+
+    def getImuWindowLayout(self, availableComPorts, comPort, baudRate) -> list:
+        """
+        Create the layout for the IMU connection window.
+
+        Args:
+            availableComPorts (list): A list of available COM ports.
+            comPort (string): Default COM port to show in COMBO box.
+            baudRate (int): Default baud rate to show in COMBO box.
+
+        Returns:
+            layout (list): Layout in the form of a list.
+        """
+        layout = [
+            [sg.Button(k='-BTN-COM-REFRESH-', button_text='', image_source='icons/refresh_icon.png',
+                       image_subsample=4, border_width=3, pad=((0, 10), (20, 0))),
+             sg.Combo(k='-COMBO-COM-PORT-', values=availableComPorts, size=7, font=st.FONT_COMBO,
+                      enable_events=True, readonly=True, default_value=comPort, pad=((0, 0), (20, 0))),
+             sg.Text('Baud Rate:', justification='right', font=st.FONT_DESCR, pad=((20, 0), (20, 0))),
+             sg.Combo(k='-COMBO-BAUD-RATE-', values=c.COMMON_BAUD_RATES, size=7, font=st.FONT_COMBO,
+                      enable_events=True, readonly=True, default_value=baudRate, pad=((0, 0), (20, 0)))],
+            [sg.HSeparator(pad=((10, 10), (20, 20)))],
+            [sg.Button(k='-BTN-IMU-CONNECT-', button_text='Connect', border_width=3, font=st.FONT_BTN)]
+        ]
+
+        return layout
