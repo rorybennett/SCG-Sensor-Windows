1fa9cd95885e2ef3b8e8e84151d62555b38502cf
Rory <rorybennett123@gmail.com>
2022-08-05 08:49:43 +0100

    add constants.py

---
+++ constants.py
@@ -0,0 +1,13 @@
+# Available return rates for the IMU. The Witmotion library limits the number of return rates that are available.
+IMU_RATE_OPTIONS = [
+    "0.2Hz",
+    "0.5Hz",
+    "1Hz",
+    "2Hz",
+    "5Hz",
+    "10Hz",
+    "20Hz",
+    "50Hz",
+    "100Hz",
+    "200Hz"
+]
